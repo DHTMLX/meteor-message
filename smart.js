4ad3cbe58edb3b7a3232bc4fdda73414d94e9d9c
Stanislau Wolski <stanislau.wolski@gmail.com>
2013-01-09 17:11:19 +0300

    [add] smart package definition

---
+++ smart.js
@@ -0,0 +1,8 @@
+{
+  "name": "dhtmlx-message",
+  "description": "Custom messages, alerts and confirmations",
+  "homepage": "https://github.com/DHTMLX/meteor-message",
+  "author": "Stanislav Wolski (http://dhtmlx.com)",
+  "version": "1.0",
+  "git": "https://github.com/DHTMLX/meteor-message.git"
+}
